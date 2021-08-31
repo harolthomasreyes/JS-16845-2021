8269260f35c883b67db4cbdfca606469bcc93366
Harold Reyes <haroldthomasreyes@gmail.com>
2021-08-30 22:39:59 -0300

    clase 1

---
+++ Clase 1/Tipos de datos.js
@@ -0,0 +1,17 @@
+//Tipos de datos
+//Texto: se usan comillas simple o dobles para repre: ('hola') o ("Adios")
+//Numeros enteros: numeros directos 10
+//Numeros Decimales: numeros directos div por coma o punto ejem: 10.67
+//Booleano: solo puede ser verdadero o false (true o false)
+
+//Como declarar un dato
+// 1) var
+var texto = "hola mundo";
+console.log("valor de la variable texto:", texto);
+// 2) let
+//Nueva palabra: Concatenar: unir texto o cualquier tipo de dato
+let numeroYTexto = "mi edad es " + 30; //automaticamente retorna texto
+console.log("valor de la variable numeroYTexto:", numeroYTexto);
+// 3) cosnt
+const soloNumero = 10 + 30; //automaticamente retorna un numero dado que numero mas numero es una operacion logico aritmetica
+console.log("valor de la variable soloNumero:", soloNumero);
---
+++ Clase 1/prompt.js
@@ -0,0 +1,3 @@
+var ingresoUsuario = prompt("ingresa tu nombre:");
+//Por si lo ejecutas desde el backend
+console.log("su nombre es:", ingresoUsuario);
