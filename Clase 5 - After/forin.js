500da74e89ac2a6575164df872a63bf313433b03
Harold Reyes <haroldthomasreyes@gmail.com>
2021-09-14 19:40:31 -0300

    clase 5 - after

---
+++ Clase 5 - After/forin.js
@@ -0,0 +1,8 @@
+//Conseguir todas las propiedades de un objeto
+for (propertyName in persona) {
+  console.log(propertyName);
+}
+//Conseguir todos los valores de un objeto
+for (propertyName in persona) {
+  console.log(persona[propertyName]);
+}
